e5382542445b5582dd6d61645e01ead368f4de68
drsacer <drsacer@gmail.com>
2021-09-20 16:53:08 +0200

    Stage 2/6

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -1,9 +1,29 @@
 package machine;
+import  java.util.Scanner;
 
 public class CoffeeMachine {
+
+    static int water = 200;
+    static int milk = 50;
+    static int coffeeBeans = 15;
+
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        //stage 1
         makingCofee();
+
+        //stage 2
+        System.out.println("Write how many cups of coffee you will need:");
+        int beans = scanner.nextInt();
+        calculatingIngredient(beans);
+        // pitanja : napraviti u mainu unos broja šalica pa poslati kao argument ?
+        //           voda, mlijeko i zrna kave staviti kao statičke varijable iznad maina ?
+        //           promijeniti ime argumenta u funkciji (bean) ? koristiti this ?
+
+
+
         
     }
     public static void makingCofee (){
@@ -14,5 +34,12 @@ public class CoffeeMachine {
                 "Pouring coffee into the cup\n" +
                 "Pouring some milk into the cup\n" +
                 "Coffee is ready!");
+    }
+
+    public static void calculatingIngredient (int beans){
+        System.out.println("For" + beans +  " cups of coffee you will need:\n" +
+                beans * 200 +  " ml of water\n" +
+                beans * 50  +  " ml of milk\n" +
+                beans * 15  +  " g of coffee beans");
     }
 }
