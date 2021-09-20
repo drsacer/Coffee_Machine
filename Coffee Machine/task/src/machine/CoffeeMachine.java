225f0ce2bae4c66e201878c238cc0a4da2e463a9
drsacer <drsacer@gmail.com>
2021-09-20 16:52:03 +0200

    Stage 1/6

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,18 @@
+package machine;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        
+        makingCofee();
+        
+    }
+    public static void makingCofee (){
+        System.out.println("Starting to make a coffee\n" +
+                "Grinding coffee beans\n" +
+                "Boiling water\n" +
+                "Mixing boiled water with crushed coffee beans\n" +
+                "Pouring coffee into the cup\n" +
+                "Pouring some milk into the cup\n" +
+                "Coffee is ready!");
+    }
+}
